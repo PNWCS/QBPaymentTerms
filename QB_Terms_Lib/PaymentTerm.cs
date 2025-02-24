5fc82dcebca4249a7fd4ffee2330528d7df8abd5
KosuruB <bkosuru@pnw.edu>
2025-02-24 00:26:32 +0000

    used default constructor

--- QB_Terms_Lib/PaymentTerm.cs
+++ QB_Terms_Lib/PaymentTerm.cs
@@ -1,16 +1,10 @@
 ﻿namespace QB_Terms_Lib
 {
-    public class PaymentTerm
+    public class PaymentTerm(string qbID, string qbRev, string name, int companyID)
     {
-        public string QB_ID { get; set; }
-        public string QB_Rev { get; set; }
-        public string Name { get; set; }
-        public int Company_ID { get; set; }
-        public PaymentTerm(string qbID, string qbRev, string name, int companyID) {
-            QB_ID = qbID;
-            QB_Rev = qbRev;
-            Name = name; 
-            Company_ID = companyID;
-        }
+        public string QB_ID { get; set; } = qbID;
+        public string QB_Rev { get; set; } = qbRev;
+        public string Name { get; set; } = name;
+        public int Company_ID { get; set; } = companyID;
     }
 }
